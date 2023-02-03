55e19d593ccb011901335b11a2a44606b64303de
pcpengchang <1767191006@qq.com>
2023-02-03 22:28:01 +0800

    Update GitDemoApplication.java

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -9,6 +9,9 @@ public class GitDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
         System.out.println("git 03 master pull");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 
 }
