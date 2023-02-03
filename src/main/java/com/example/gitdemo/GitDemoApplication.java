89f07f4f050b540deea2aeb9e08dc4f44915354b
“pcpengchang” <1767191006@qq.com>
2023-02-03 21:59:02 +0800

    commit 03

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -8,7 +8,7 @@ public class GitDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
-        System.out.println("git 02");
+        System.out.println("git 02 master");
     }
 
 }
